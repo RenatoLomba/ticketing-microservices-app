a45c28c3d9e9475c6d7342aef1555513a86e6a87
Renato Lomba <rntlomba@gmail.com>
2022-08-04 21:34:56 -0300

    Ticket slug

--- tickets-service/src/services/tickets.service.ts
+++ tickets-service/src/services/tickets.service.ts
@@ -1,3 +1,5 @@
+import slugify from 'slugify'
+
 import { ForbiddenException, Injectable } from '@nestjs/common'
 import { Prisma } from '@prisma/client'
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime'
@@ -8,19 +10,30 @@ import { PrismaService } from '../database/prisma/prisma.service'
 export class TicketsService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async createTicket({ price, title, userId }: Prisma.TicketCreateInput) {
+  async createTicket({
+    price,
+    title,
+    userId,
+  }: Omit<Prisma.TicketCreateInput, 'slug'>) {
+    const slug = slugify(title, {
+      lower: true,
+      trim: true,
+      strict: true,
+    })
+
     return this.prisma.ticket
       .create({
         data: {
           price,
           title,
           userId,
+          slug,
         },
       })
       .catch((error) => {
         if (error instanceof PrismaClientKnownRequestError) {
           if (error.code === 'P2002') {
-            throw new ForbiddenException('Duplicate ticket title')
+            throw new ForbiddenException('Duplicate ticket slug')
           }
         }
 
