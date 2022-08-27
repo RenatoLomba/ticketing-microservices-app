2b8ff56ee83070a61534f442fedec7e96d705f05
Renato Lomba <rntlomba@gmail.com>
2022-08-27 17:34:31 -0300

    Manual ack mode

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,10 +10,16 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
   console.log('Listener connected to NATS...')
 
+  const options = stan.subscriptionOptions()
+    // Event must be manually acknowledged, otherwise NATS will send the event again
+    .setManualAckMode(true)
+
   // Subscribe to a channel ticket:created
   // Queue group are for that the event got send to just one member of the queue group at a time
   // Other listeners outside the queue group will receive the event as well
-  const subscription = stan.subscribe('ticket:created', 'listener-queue-group')
+  const subscription = stan.subscribe(
+    'ticket:created', 'listener-queue-group', options
+  )
 
   // Listen to the message event on the subscription
   subscription.on('message', (msg: Message) => {
@@ -23,5 +29,9 @@ stan.on('connect', () => {
 
     console.log('Message sequence: ', msg.getSequence())
     console.log('Message data: ', JSON.parse(data))
+
+    // Manually acknowledge that the event is processed successfully, 
+    // otherwise it would be sent again until it is acknowledged
+    msg.ack()
   })
 })
