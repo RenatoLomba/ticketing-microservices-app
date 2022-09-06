1b40afb653b64607416e9b680f2783bf80de3db0
Renato Lomba <rntlomba@gmail.com>
2022-09-06 20:09:41 -0300

    NATS ENV VARIABLES

--- tickets-service/src/events/events.module.ts
+++ tickets-service/src/events/events.module.ts
@@ -1,20 +1,26 @@
 import { Stan } from 'node-nats-streaming'
 
 import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 import { TicketCreatedPublisher } from './publishers/ticket-created.publisher'
 import { TicketUpdatedPublisher } from './publishers/ticket-updated.publisher'
 
 @Module({
+  imports: [ConfigModule],
   providers: [
     {
+      inject: [ConfigService],
       provide: 'NATS_STREAMING_CONNECTION',
-      useFactory: async () => {
+      useFactory: async (config: ConfigService) => {
         const nats = await import('node-nats-streaming')
+        const natsClusterId = config.get<string>('NATS_CLUSTER_ID')
+        const natsClientId = config.get<string>('NATS_CLIENT_ID')
+        const natsUrl = config.get<string>('NATS_URL')
 
         return new Promise<Stan>((resolve, reject) => {
-          const stan = nats.connect('ticketing', 'tickets-service-publisher', {
-            url: 'http://nats-srv:4222',
+          const stan = nats.connect(natsClusterId, natsClientId, {
+            url: natsUrl,
           })
 
           stan.on('error', (err) => {
