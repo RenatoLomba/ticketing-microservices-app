a417de75d11791f8303543463b063d57ee0febc1
Renato Lomba <rntlomba@gmail.com>
2022-09-09 17:29:14 -0300

    GetUserOrdersHandler unit test

---
+++ orders-service/src/handlers/__tests__/get-user-orders.handler.spec.ts
@@ -0,0 +1,104 @@
+import { faker } from '@faker-js/faker'
+import { ConfigService } from '@nestjs/config'
+
+import { authToken } from '../../../test/utils/auth-token'
+import { createNestApp } from '../../../test/utils/create-nest-app'
+import { PrismaService } from '../../database/prisma/prisma.service'
+import { ProductsRepository } from '../../database/repositories/products.repository'
+import { CreatePendingOrderHandler } from '../create-pending-order.handler'
+import { CreateProductHandler } from '../create-product.handler'
+import { GetUserOrdersHandler } from '../get-user-orders.handler'
+
+describe('GetUserOrdersHandler', () => {
+  let handler: GetUserOrdersHandler
+  let config: ConfigService
+  let productsRepository: ProductsRepository
+  let createOrder: CreatePendingOrderHandler
+  let prisma: PrismaService
+
+  beforeEach(async () => {
+    const app = await createNestApp()
+
+    handler = app.get(GetUserOrdersHandler)
+    config = app.get(ConfigService)
+    productsRepository = app.get(ProductsRepository)
+    createOrder = app.get(CreatePendingOrderHandler)
+    prisma = app.get(PrismaService)
+
+    await app.init()
+  })
+
+  describe('execute', () => {
+    let currentUserId: string
+
+    beforeEach(() => {
+      jest.clearAllMocks()
+
+      const { user } = authToken(config)
+      currentUserId = user.id
+    })
+
+    it('should return an empty array if user does not have any orders', async () => {
+      expect(await handler.execute({ userId: currentUserId })).toEqual([])
+    })
+
+    it('should return an empty array if user does not have any orders even if other users have', async () => {
+      const externalId = faker.datatype.uuid()
+      const orderOwnerId = faker.datatype.uuid()
+
+      await prisma.product.create({
+        data: {
+          id: faker.datatype.uuid(),
+          externalId,
+          price: 20.5,
+          title: 'Ticket 1',
+        },
+      })
+
+      await createOrder.execute({
+        externalId,
+        userId: orderOwnerId,
+      })
+
+      const ordersCount = (await prisma.order.findMany()).length
+
+      expect(ordersCount).toEqual(1)
+      expect(await handler.execute({ userId: currentUserId })).toEqual([])
+    })
+
+    it('should return an array with the orders that a user created', async () => {
+      const externalId = faker.datatype.uuid()
+      const externalId2 = faker.datatype.uuid()
+
+      await prisma.product.create({
+        data: {
+          id: faker.datatype.uuid(),
+          externalId,
+          price: 20.5,
+          title: 'Ticket 1',
+        },
+      })
+      await prisma.product.create({
+        data: {
+          id: faker.datatype.uuid(),
+          externalId: externalId2,
+          price: 20.5,
+          title: 'Ticket 2',
+        },
+      })
+
+      await createOrder.execute({
+        externalId,
+        userId: currentUserId,
+      })
+      await createOrder.execute({
+        externalId: externalId2,
+        userId: currentUserId,
+      })
+
+      expect((await handler.execute({ userId: currentUserId })).length).toEqual(
+        2,
+      )
+    })
+  })
+})
